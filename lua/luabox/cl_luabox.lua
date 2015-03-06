812a733c69217b3422cd9def1a26d30fbd38837d
Nathan Beals <nathan_beals@hotmail.com>
2015-03-06 14:03:57 -0500

    Removed un needed things

--- lua/luabox/cl_luabox.lua
+++
@@ -1,2 +0,0 @@
---Copyright 2014 Nathan Beals
-print("luabox client inited")
--- lua/luabox/sv_luabox.lua
+++
@@ -1,9 +0,0 @@
---Copyright 2014 Nathan Beals
-
-local function test()
-	print("little lamb :(")
-end
-
---local func = luabox.SandBox( test )
-
---func()
